ea41e9c08d39a07aa3cf2b209ee7b0c484afd2ec
Samuel-Oyedeji <oyedejisamuel05@gmail.com>
2025-02-11 10:32:18 +0100

    Update main.tf

--- Day 6 - AWS EC2 Instance with Terraform/main.tf
+++ Day 6 - AWS EC2 Instance with Terraform/main.tf
@@ -1,1 +1,14 @@
+##AWS EC2 with terraform
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
 
+resource "aws_instance" "<name_of_server>" {
+  ami           = "ami-088b41ffb0933423f"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "<tag-your-server>"
+  }
+}
